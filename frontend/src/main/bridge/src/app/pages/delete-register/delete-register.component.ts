dd6e714d4672865dc15d9daa87cdd715a8545028
daaaniel6 <danielgonzalezdagg@gmail.com>
2021-07-27 11:11:02 -0600

    borrando carpetas

---
+++ frontend/src/main/bridge/src/app/pages/delete-register/delete-register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-register',
+  templateUrl: './delete-register.component.html',
+  styleUrls: ['./delete-register.component.scss']
+})
+export class DeleteRegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
