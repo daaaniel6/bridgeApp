0ed4a34daa78926ebc3766301b424ed93db13a87
daaaniel6 <danielgonzalezdagg@gmail.com>
2021-07-31 13:55:28 -0600

    nuevo proyecto maven

---
+++ bridgeApp/src/main/bridge/src/app/app.module.ts
@@ -0,0 +1,95 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PanelMenuComponent } from './components/panel-menu/panel-menu.component';
+
+// primeng
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { MenubarComponent } from './components/menubar/menubar.component';
+import { MenubarModule } from 'primeng/menubar';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+// sirve para el ng-tamplate
+import { SharedModule } from 'primeng/api';
+import { AvatarModule } from 'primeng/avatar';
+import { BadgeModule } from 'primeng/badge';
+import { MenuModule } from 'primeng/menu';
+// sirve para el meni del usuario
+import { RippleModule } from 'primeng/ripple';
+//mensaejes
+import { ToastModule } from 'primeng/toast';
+
+import { MessagesModule } from 'primeng/messages';
+import { MessageModule } from 'primeng/message';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Page404Component } from './pages/page404/page404.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+//carousel
+import { CarouselModule } from 'primeng/carousel';
+
+import { NewRegisterComponent } from './pages/new-register/new-register.component';
+import { EditRegisterComponent } from './pages/edit-register/edit-register.component';
+import { DeleteRegisterComponent } from './pages/delete-register/delete-register.component';
+
+//charts
+// import { ChartsModule } from 'ng2-charts';
+// import { ChartModule } from 'primeng/chart';
+// import { BarChartComponent } from './components/bar-chart/bar-chart.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+
+import { PieChartComponent } from './components/pie-chart/pie-chart.component';
+
+// dialogos
+import { DialogModule } from 'primeng/dialog';
+
+//tablas
+import { TableModule } from 'primeng/table';
+import { BridgesTableComponent } from './components/bridges-table/bridges-table.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PanelMenuComponent,
+    MenubarComponent,
+    DashboardComponent,
+    Page404Component,
+    CarouselComponent,
+    NewRegisterComponent,
+    EditRegisterComponent,
+    DeleteRegisterComponent,
+    //BarChartComponent,
+    PieChartComponent,
+    BridgesTableComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    PanelMenuModule,
+    MenubarModule,
+    ButtonModule,
+    InputTextModule,
+    SharedModule,
+    AvatarModule,
+    BadgeModule,
+    MenuModule,
+    RippleModule,
+    MessagesModule,
+    MessageModule,
+    CarouselModule,
+    ToastModule,
+    //ChartsModule,
+    //ChartModule,
+    NgxChartsModule,
+    DialogModule,
+    TableModule,
+  ],
+  exports: [NgxChartsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ bridgeApp/src/main/bridge/src/app/components/menubar/menubar.component.ts
@@ -0,0 +1,180 @@
+import { Component, OnInit } from '@angular/core';
+
+// primeng
+import { MenuItem, MessageService, PrimeNGConfig } from 'primeng/api';
+import { Message } from 'primeng//api';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-menubar',
+  templateUrl: './menubar.component.html',
+  styleUrls: ['./menubar.component.scss'],
+  providers: [MessageService],
+})
+export class MenubarComponent implements OnInit {
+  items: MenuItem[] = [];
+  itemsUser: MenuItem[] = [];
+  image: boolean = false;
+  msgs1: Message[] = [
+    { severity: 'success', summary: 'Success', detail: 'Message Content' },
+    { severity: 'info', summary: 'Info', detail: 'Message Content' },
+    { severity: 'warn', summary: 'Warning', detail: 'Message Content' },
+    { severity: 'error', summary: 'Error', detail: 'Message Content' },
+  ];
+
+  constructor(
+    private messageService: MessageService,
+    private primengConfig: PrimeNGConfig,
+    private route: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.primengConfig.ripple = true;
+
+    this.itemsUser = [
+      {
+        label: 'Opciones',
+        items: [
+          {
+            label: 'Mi perfil',
+            icon: 'pi pi-refresh',
+            command: (event) => {
+              this.redirectToPage('/404');
+            },
+          },
+          {
+            label: 'Notificaciones',
+            icon: 'pi pi-external-link',
+            command: (click) => {
+              this.redirectToPage('/404');
+            },
+          },
+        ],
+      },
+      {
+        label: 'Navegar',
+        items: [
+          {
+            label: 'Salir',
+            icon: 'pi pi-times',
+            url: 'http://google.com',
+          },
+          {
+            label: 'Router',
+            icon: 'pi pi-upload',
+            command: (click) => {
+              this.delete();
+            },
+          },
+        ],
+      },
+    ];
+
+    this.items = [
+      {
+        label: 'Registros',
+        icon: 'pi pi-fw pi-file',
+        items: [
+          {
+            label: 'Nuevo',
+            icon: 'pi pi-fw pi-plus',
+            items: [
+              {
+                label: 'Registro de puente existente',
+                icon: 'pi pi-fw pi-bookmark',
+                command: (click) => {
+                  this.addMessage();
+                },
+              },
+              {
+                label: 'Nuevo puente',
+                icon: 'pi pi-fw pi-video',
+                command: (click) => {
+                  this.redirectToPage('/register');
+                },
+              },
+            ],
+          },
+          {
+            label: 'En proceso',
+            icon: 'pi pi-fw pi-pencil',
+          },
+          {
+            label: 'Eliminar',
+            icon: 'pi pi-fw pi-trash',
+            command: (click) => {
+              this.redirectToPage('/algo');
+            },
+          },
+          {
+            separator: true,
+          },
+          {
+            label: 'Export',
+            icon: 'pi pi-fw pi-external-link',
+          },
+        ],
+      },
+
+      {
+        label: 'Consultar',
+        icon: 'pi pi-fw pi-calendar',
+        items: [
+          {
+            label: 'Puente',
+            icon: 'pi pi-fw pi-sitemap',
+            items: [
+              {
+                label: 'Mis puentes',
+                icon: 'pi pi-fw pi-calendar-minus',
+              },
+              {
+                label: 'Todos los puentes',
+                icon: 'pi pi-fw pi-calendar-plus',
+              },
+            ],
+          },
+          {
+            separator: true,
+          },
+          {
+            label: 'Graficas',
+            icon: 'pi pi-fw pi-calendar-times',
+            command: (click) => {
+              this.redirectToPage('/');
+            },
+          },
+        ],
+      },
+    ];
+  }
+
+  update() {
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Success',
+      detail: 'Data Updated',
+    });
+  }
+
+  delete() {
+    this.messageService.add({
+      severity: 'warn',
+      summary: 'Delete',
+      detail: 'Data Deleted',
+    });
+  }
+
+  redirectToPage(page: string) {
+    this.route.navigate([page]);
+  }
+
+  addMessage() {
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Service Message',
+      detail: 'Via MessageService',
+      sticky: true,
+    });
+  }
+}
